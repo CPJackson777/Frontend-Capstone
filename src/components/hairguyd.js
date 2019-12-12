02ca833e574530a9341d625fc72c5d94f8a84a83
Charles Jackson <cpjackson572@gmail.com>
2019-12-12 17:05:15 -0600

    changed Navbar.js to camel case

--- src/components/hairguyd.js
+++ src/components/hairguyd.js
@@ -1,27 +1,42 @@
 import React, { Component } from "react";
 import ApplicationViews from "./ApplicationViews";
+import NavBar from './nav/NavBar'
 
 
 class HairGuyd extends Component {
 
-    isSignedup = () => localStorage.getItem("credentials") !== null
+  //****************************** 
+  // user doesn't exist by default
+  //******************************
+  state = {
+    user: false
+  }
 
-    setUser = (signupObj) => {
-        // Set Store Email and password in local storage
-        localStorage.setItem(
-          "credentials",
-          JSON.stringify(signupObj)
-        )
-        this.setState({
-          user: this.isSignedup()
-        });
-      }
+  //*****************************************************************************************
+  // Authentication: isSignedUp checks if credentials are in local storage returns true/false
+  //*****************************************************************************************
+  isSignedup = () => localStorage.getItem("credentials") !== null
+
+  //**********************************************
+  // Set Store Email and password in local storage
+  //**********************************************
+  setUser = (signupObj) => {
+    localStorage.setItem(
+      "credentials",
+      JSON.stringify(signupObj)
+    )
+    this.setState({
+      user: this.isSignedup()
+    });
+  }
 
 
   render() {
     return (
-
-          <ApplicationViews setUser={this.setUser}/>
+      <>
+      <NavBar user={this.state.user} clearUser={this.clearUser} />
+      <ApplicationViews setUser={this.setUser} />
+      </>
     );
   }
 }

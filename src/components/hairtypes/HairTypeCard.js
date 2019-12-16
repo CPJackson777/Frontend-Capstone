8483be79b25ca4950296649903885bbfd6879c06
Charles Jackson <cpjackson572@gmail.com>
2019-12-15 19:20:15 -0600

    pics showing now that I'm using jpeg files. Not sure why svg files won't work.

--- src/components/hairtypes/HairTypeCard.js
+++ src/components/hairtypes/HairTypeCard.js
@@ -7,13 +7,13 @@ class HairTypeCard extends Component {
       <div className="card">
         <div className="card-content">
           <picture>
-            <img src={require('./coily.svg')} alt="Coily Hair Pic" />
+            <img src={require('./Paul gives Ringo props.jpg')} alt="Coily Hair Pic" />
           </picture>
           {/* <h3>Name: <b>{this.props.animal.name}</b></h3>
           <p>Breed: {this.props.animal.breed}</p> */}
          
         
-          <Link to={`/hairtypes/${this.props.hairtype.id}`}><button>Details</button></Link>
+          <Link to={`/hairtypes/${this.props.hairtype.id}`}><button>Tutorials</button></Link>
         </div>
       </div>
     );
--- src/components/hairtypes/HairTypeList.js
+++ src/components/hairtypes/HairTypeList.js
@@ -1,10 +1,10 @@
 //  PURPOSE: Initiate the AnimalManager getAll() call, hold on to the returned data, and then render the <AnimalCard /> component for each animal. When the data is returned, we can hold on to it by placing it in the component's state.
 
-
 import React, { Component } from 'react'
 //import the components we will need
 import HairTypeCard from './HairTypeCard'
 import ApiManager from '../../modules/ApiManager'
+
 
 class HairTypeList extends Component {
   //define what this component needs to render
@@ -15,7 +15,7 @@ class HairTypeList extends Component {
   componentDidMount() {
     console.log("HAIRTYPE LIST: ComponentDidMount");
     //getAll from ApiManager and hang on to that data; put it in state
-    ApiManager.getAll('hairtypes')  //If using this with generic fetch call, plug in parameter like this = ApiManager.getAll('animals')
+    ApiManager.getAll('hairtypes')  
       .then((hairtypesArray) => {
         this.setState({
           hairtypes: hairtypesArray

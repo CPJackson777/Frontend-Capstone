707c9489ef385ee12fa734621a942fd71491aa99
Charles Jackson <cpjackson572@gmail.com>
2020-01-06 20:39:48 -0600

    trying to add full page background pic

--- src/components/welcome/Welcome.js
+++ src/components/welcome/Welcome.js
@@ -1,22 +1,25 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
-import vet1 from './vet1.png';
+import HairGuydLogoLarge from './HairGuydLogoLarge.png';
+import site-bg from './site-bg.jpg'
 import './Welcome.css'
+import { ButtonGroup } from 'react-bootstrap';
 
 class Welcome extends Component {
     render() {
         return (
             <>
                 <div id="welcomeBackground" >
-
+                <img className="bg-image" src={site-bg}></img>
                     
-                    <img className="welcome-logo" src={vet1}></img>
+                    <img className="welcome-logo" src={HairGuydLogoLarge}></img>
                     
-                    <div id="signUpButton">
-                        <Link to={`/signup`}><button>Sign Up</button></Link>
+                    <div className="auth-button">
+                        <Link to={`/signup`}><button className="button is-medium is-rounded is-info is-outlined">Sign Up</button></Link>
                     </div>
-                    <div id="loginButton">
-                        <Link to={`/login`}><button>Login</button></Link>
+                    <br></br>
+                    <div className="auth-button">
+                        <Link to={`/login`}><button className="button is-medium is-rounded is-warning">Login</button></Link>
                     </div>
 
                 </div>
